8e6477292592fdeaa0c0213bf280f8c3b3b9a20a
Jamie <G00419525@atu.ie>
2023-11-13 16:57:50 +0000

    Working try/catch method.

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -4,22 +4,29 @@ import java.util.Scanner;
 
 
 public class UsingTryCatch {
-    public static void main(String[] args){
-        System.out.println("Please enter a number between 0 - 9: ");
+    public static void main(String[] args) {
         Scanner myscan = new Scanner(System.in);
+       while(true) {
+           try {
+               System.out.println("\nPlease enter a number between 0 - 9: ");
 
-            //Read user input as a string
-            String input = myscan.nextLine();
+               //Read user input as a string
+               String input = myscan.nextLine();
 
-            //parse the input to an integer
-            int myNum = Integer.parseInt(input);
+               //parse the input to an integer
+               int myNum = Integer.parseInt(input);
 
-            if(myNum >= 0 && myNum <= 9){
-                System.out.println("You entered " + myNum);
-            }
-            else{
-                System.out.println("Not a valid input, Please enter a number between 0 - 9");
-            }
+               if (myNum >= 0 && myNum <= 9) {
+                   System.out.println("You entered " + myNum);
+                   break;
+               } else {
+                   System.out.println("Not a valid input, Please enter a number between 0 - 9");
+               }
+           } catch (NumberFormatException e) {
+               System.out.println("Not a valid input, Please enter a number between 0 - 9");
+           }
+       }
+       }
     }
-}
+
 
